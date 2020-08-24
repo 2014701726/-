01b83cadb2baa359e5a2dc21ed9be28a81b8f148
s289260 <289260318@qq.com>
2020-08-24 13:24:42 +0800

    配置eureka服务注册发现中心

--- motianlun-eureka/src/main/java/whj/nb/motianluneureka/MotianlunEurekaApplication.java
+++ motianlun-eureka/src/main/java/whj/nb/motianluneureka/MotianlunEurekaApplication.java
@@ -2,12 +2,13 @@ package whj.nb.motianluneureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class MotianlunEurekaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MotianlunEurekaApplication.class, args);
     }
-
 }
