377c01167b35a198259adaf9e9c47da479c5e41d
s289260 <289260318@qq.com>
2020-08-24 10:24:25 +0800

    第一次提交

---
+++ motianlun-eureka/src/main/java/whj/nb/motianluneureka/MotianlunEurekaApplication.java
@@ -0,0 +1,13 @@
+package whj.nb.motianluneureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotianlunEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotianlunEurekaApplication.class, args);
+    }
+
+}
