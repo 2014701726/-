e0fb9d836c1bf084ba3136836f0c1637a64d6e1e
s289260 <289260318@qq.com>
2020-08-25 09:51:08 +0800

    添加前端接收返回值类型结构

--- motianlun-resource/src/main/java/whj/nb/vo/ResultVO.java
+++ motianlun-resource/src/main/java/whj/nb/vo/ResultVO.java
@@ -5,5 +5,53 @@ package whj.nb.vo;
  * @Date: 2020/8/25 9:44
  */
 
-public class ResultVO {
+public class ResultVO<T> {
+
+    private Integer code;
+    private String msg;
+    private T t;
+
+
+    public ResultVO(Integer code, String msg, T t) {
+        this.code = code;
+        this.msg = msg;
+        this.t = t;
+    }
+
+    public ResultVO() {
+    }
+
+
+    @Override
+    public String toString() {
+        return "ResultVO{" +
+                "code=" + code +
+                ", msg='" + msg + '\'' +
+                ", t=" + t +
+                '}';
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getT() {
+        return t;
+    }
+
+    public void setT(T t) {
+        this.t = t;
+    }
 }
