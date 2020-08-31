37f066cfbb235de140ec67124d7e113debfff28b
望健 <2014701726@qq.com>
2020-08-31 21:11:58 +0800

    测试完成合并

--- motianlun-eureka/src/main/java/whj/nb/performance/MotianlunEurekaApplication.java
+++ motianlun-eureka/src/main/java/whj/nb/performance/MotianlunEurekaApplication.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:motianlun-eureka/src/main/java/whj/nb/performance/MotianlunEurekaApplication.java
 package whj.nb.performance;
-=======
-package whj.nb.goodsdetail;
->>>>>>> dev-long:motianlun-eureka/src/main/java/whj/nb/goodsdetail/MotianlunEurekaApplication.java
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- motianlun-eureka/src/test/java/whj/nb/performance/MotianlunEurekaApplicationTests.java
+++ motianlun-eureka/src/test/java/whj/nb/performance/MotianlunEurekaApplicationTests.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:motianlun-eureka/src/test/java/whj/nb/performance/MotianlunEurekaApplicationTests.java
 package whj.nb.performance;
-=======
-package whj.nb.goodsdetail;
->>>>>>> dev-long:goods-detail/src/test/java/whj/nb/goodsdetail/MotianlunEurekaApplicationTests.java
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
